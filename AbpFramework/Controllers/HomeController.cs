486d24445eabbeaf834566e4185b816320114fa1
louder <360894898@qq.com>
2018-01-08 10:12:04 +0800

    test

--- AbpFramework/Controllers/HomeController.cs
+++ AbpFramework/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace AbpFramework.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {   
             return View();
         }
 
